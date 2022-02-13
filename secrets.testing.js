e2162730e3caaa689a89c517955803b4391b426d
noah <90880131+noahsmiths@users.noreply.github.com>
2022-02-12 21:24:03 -0500

    Update secrets.testing.js

--- secrets.testing.js
+++ secrets.testing.js
@@ -1,5 +1,6 @@
 export default {
-  //PROJECT_ID_MAINNET: 'mainnetWBBLrftODbkS3tbk6krulzawMqjN8cQF',
-  PROJECT_ID_MAINNET: 'mainnetUxZ1oGgRnSRbrsR0DUuyNY2hCL5tGqBy',
-  PROJECT_ID_TESTNET: 'testnetWl6ehdZnbuV5VyjTfnuC3J5IQ55OdPAc',
+    //PROJECT_ID_MAINNET: 'mainnetWBBLrftODbkS3tbk6krulzawMqjN8cQF',
+    //PROJECT_ID_TESTNET: 'testnetWl6ehdZnbuV5VyjTfnuC3J5IQ55OdPAc',
+    PROJECT_ID_MAINNET: 'mainnetUxZ1oGgRnSRbrsR0DUuyNY2hCL5tGqBy',
+    PROJECT_ID_TESTNET: 'testnetJe6W7FM1Jwkh0PxNMZt9OzNND3T1mS1T',
 };
